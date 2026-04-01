e574caf0d80c325d0a0404317c924619c757c208
valentin <bellberger45@gmail.com>
2026-04-01 15:38:55 -0300

    feat: common card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,13 @@
-<template></template>
+<script setup lang="ts"></script>
+
+<template>
+  <main></main>
+</template>
+
+<style scoped>
+main {
+  margin: 200px auto;
+  width: 90%;
+  max-width: 1200px;
+}
+</style>
